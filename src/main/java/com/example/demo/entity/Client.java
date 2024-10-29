644c42e5c84f166b8384be0ecfe31eb49d14e6bd
HuNGuM <96438485+HuNGuM@users.noreply.github.com>
2024-10-29 12:05:48 +0100

    Client table and liquibase added

---
+++ src/main/java/com/example/demo/entity/Client.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "fio", length = 90, nullable = false)
+    private Long fio;
+    @Column(name = "login", length = 90, nullable = false, unique = true)
+    private Long login;
+    @Column(name = "password", length = 90, nullable = false)
+    private Long password;
+
+}
